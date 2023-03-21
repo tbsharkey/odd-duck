75401b7f2bc6f70fadb414dd074b575ef7182d89
Tom Sharkey <tbsharkey@gmail.com>
2023-03-20 23:53:23 -0700

    Added basic structure and some JS

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function Product(name, filePath) {
+  this.name = name;
+  this.filePath = filePath;
+  this.timesShown = 0;
+  this.timesClicked = 0;
+}
+
+const products = [
+  
+];
+
+let currentProducts = [];
+let totalRounds = 25;
+let currentRound = 0;
+
+const imageFiles = [
+  'bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg',
+  'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg',
+  'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png',
+  'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg'
+];
